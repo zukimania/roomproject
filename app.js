6243e99a825f3d559bb7e85478d9e7fce6311d02
zukimania <eayodele585@gmail.com>
2023-05-12 23:41:24 +0100

    first commit

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navToggle = document.querySelector(".nav-toggle");
+const line1 = document.querySelector(".line-1");
+const line2 = document.querySelector(".line-2");
+const line3 = document.querySelector(".line-3");
 const navLinks = document.querySelector(".nav-links");
 
-hamburgerMenu.addEventListener("click", () => {
-  hamburgerMenu.classList.toggle("active");
+navToggle.addEventListener("click", () => {
+  line1.classList.toggle("active");
+  line2.classList.toggle("active");
+  line3.classList.toggle("active");
   navLinks.classList.toggle("active");
 });
 
-window.addEventListener("resize", () => {
-  if (window.innerWidth > 768) {
-    navLinks.classList.remove("active");
-    hamburgerMenu.classList.remove("active");
-  }
-});
+ window.addEventListener("resize", () => {
+if (window.innerWidth > 768) {
+navLinks.classList.remove("active");
+line1.classList.remove("active");
+line2.classList.remove("active");
+line3.classList.remove("active");
+}
+ });
